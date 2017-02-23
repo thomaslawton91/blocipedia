95027c5623cb419a608cfdfa60f87ae18c407ec5
thomaslawton91 <thomaslawton91@gmail.com>
2017-02-23 10:59:18 -0500

    Create seeds with Faker.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+  User.create(
+  email: Faker::Internet.email,
+  password: Faker::Internet.password
+  )
+end
+
+users = User.all
+
+50.times do
+  Wiki.create(
+  title: Faker::Cat.breed,
+  body: Faker::Cat.name,
+  private: Faker::Boolean.boolean,
+  user: users.sample
+  )
+end
+
+me = User.create(
+email: 'thomaslawton91@gmail.com',
+password: '111111'
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
